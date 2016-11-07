9df0c65fe8e1b324b9f1600776907bf91a14cba9
Michele Mazzucco <hello@michelemazzucco.it>
2016-11-07 15:35:37 +0100

    wip

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
 const public_ip = require('public-ip')
-const fetchPosition = require('./lib/fetchPosition')
+const App = require('./lib/app')
 
 let ip = ''
 public_ip.v4().then(your_ip => ip = your_ip)
 
-fetchPosition(ip)
+let a = new App(ip)
+a.init()
