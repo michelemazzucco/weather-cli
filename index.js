783508bef7f19040a484bfbc6071402e864ea511
Michele Mazzucco <hello@michelemazzucco.it>
2016-11-11 21:19:29 +0100

    refactiong wip

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 #!/usr/bin/env node
-
 const public_ip = require('public-ip')
+const chalk = require('chalk')
 const App = require('./lib/app')
 
-let ip = ''
-public_ip.v4().then(your_ip => ip = your_ip)
+//public_ip.v4()
+//  .then((res) => {
+//    let ip = '188.218.142.123'
+//
+//    let a = new App(ip)
+//    a.run()
+//  })
+//  .catch((e) => console.log(chalk.red('✗ ' + e.message))
+//)
+
+let ip = '188.218.142.123'
 
 let a = new App(ip)
-a.init()
+a.run()
