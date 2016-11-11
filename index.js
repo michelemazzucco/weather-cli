d739630802a5b691b8724ab0887d15d9bbddac84
Michele Mazzucco <hello@michelemazzucco.it>
2016-11-11 21:32:27 +0100

    remove fake ip

--- index.js
+++ index.js
@@ -3,17 +3,12 @@ const public_ip = require('public-ip')
 const chalk = require('chalk')
 const App = require('./lib/app')
 
-//public_ip.v4()
-//  .then((res) => {
-//    let ip = '188.218.142.123'
-//
-//    let a = new App(ip)
-//    a.run()
-//  })
-//  .catch((e) => console.log(chalk.red('✗ ' + e.message))
-//)
+public_ip.v4()
+  .then((r) => {
+    let ip = r
 
-let ip = '188.218.142.123'
-
-let a = new App(ip)
-a.run()
+    let a = new App(ip)
+    a.run()
+  })
+  .catch((e) => console.log(chalk.red('✗ ' + e.message))
+)
